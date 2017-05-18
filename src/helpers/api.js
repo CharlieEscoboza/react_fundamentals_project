ec5d450ea36deb5737836460379bbc976b515542
CharlieGarcia <ing.charlie.garcia@gmail.com>
2017-05-17 20:17:02 -0400

    Add production and deploy config

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '925b9453e53850659875ac409d045768';
-const API_URL = `http://api.openweathermap.org/data/2.5/forecast/daily?q={CITY-NAME}&type=accurate&APPID=${API_KEY}&cnt=5`;
+const API_URL = `//api.openweathermap.org/data/2.5/forecast/daily?q={CITY-NAME}&type=accurate&APPID=${API_KEY}&cnt=5`;
 
 const fetchData = (cityName) => {
   const url = API_URL.replace('{CITY-NAME}', cityName);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
+const config = {
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -34,4 +35,16 @@ module.exports = {
       template: 'src/index.html'
     })
   ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  );
 }
+
+module.exports = config;
